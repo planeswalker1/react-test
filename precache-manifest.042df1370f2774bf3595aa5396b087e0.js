da777891fd9ac515f6999693e472bb434965d594
Daniel Munoz <daniel.munozdev@gmail.com>
2019-03-02 20:09:52 -0800

    Updates

---
+++ precache-manifest.042df1370f2774bf3595aa5396b087e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "98554640f1ac2b0bda5d",
+    "url": "/react-test/static/js/runtime~main.98554640.js"
+  },
+  {
+    "revision": "5d51c3cdc1633be12807",
+    "url": "/react-test/static/js/main.5d51c3cd.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-test/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d51c3cdc1633be12807",
+    "url": "/react-test/static/css/main.ce0e7570.chunk.css"
+  },
+  {
+    "revision": "ebf3af3d9cfb2be547254dc55664d617",
+    "url": "/react-test/index.html"
+  }
+];
